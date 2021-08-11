83233d241192d339ef2d51b430ef50b8f98a205e
iw0406 <hyunjilim98@gmail.com>
2021-08-11 17:42:47 +0900

    [Create] Init

---
+++ src/main/java/hacklearn/mygiftsavor/MyGiftSavorApplication.java
@@ -0,0 +1,13 @@
+package hacklearn.mygiftsavor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGiftSavorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGiftSavorApplication.class, args);
+	}
+
+}
