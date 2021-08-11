2f1636febc5b2e1d56e90eba064b823a795e37ad
iw0406 <hyunjilim98@gmail.com>
2021-08-11 20:08:44 +0900

    [Create] Exception and Handler

---
+++ src/main/java/hacklearn/mygiftsavor/infra/exception/DuplicateException.java
@@ -0,0 +1,9 @@
+package hacklearn.mygiftsavor.infra.exception;
+
+public class DuplicateException extends CustomRuntimeException {
+
+    public DuplicateException(String msg) {
+        super(msg);
+        name = "DuplicateException";
+    }
+}
---
+++ src/main/java/hacklearn/mygiftsavor/infra/exception/NoSuchDataException.java
@@ -0,0 +1,9 @@
+package hacklearn.mygiftsavor.infra.exception;
+
+public class NoSuchDataException extends CustomRuntimeException{
+
+    public NoSuchDataException(String msg){
+        super(msg);
+        name = "NoSuchDataException";
+    }
+}
