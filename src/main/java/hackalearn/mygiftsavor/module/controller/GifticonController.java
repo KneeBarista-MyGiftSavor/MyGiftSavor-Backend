01d54d745ca09f04b18126cff8241d2c3dcd2ba0
iw0406 <hyunjilim98@gmail.com>
2021-08-15 15:54:14 +0900

    [Update] Cors...

--- src/main/java/hackalearn/mygiftsavor/module/controller/GifticonController.java
+++ src/main/java/hackalearn/mygiftsavor/module/controller/GifticonController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 
 import static hackalearn.mygiftsavor.module.model.dto.GifticonDtos.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Slf4j
 @RequiredArgsConstructor
 @RestController
--- src/main/java/hackalearn/mygiftsavor/module/controller/HomeController.java
+++ src/main/java/hackalearn/mygiftsavor/module/controller/HomeController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.List;
 
 import static hackalearn.mygiftsavor.module.model.dto.GifticonDtos.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Slf4j
 @RequiredArgsConstructor
 @RestController
